a25a0f38e4a40559ad69b533999107668432fcc9
ayushjhax <ayushkmrjha@yahoo.com>
2025-03-03 19:08:10 +0530

    Refactor components and update text for clarity and consistency

--- frontend/src/components/aroraBack.tsx
+++ frontend/src/components/aroraBack.tsx
@@ -20,19 +20,19 @@ export function AuroraBack() {
           className="flex flex-col gap-4 items-start justify-center px-8 text-left w-2/3"
         >
           <div className="text-2xl md:text-5xl font-bold dark:text-white">
-            Twitter Tournament
+          Enter the Arena
           </div>
           <div className="flex items-center">
             <div className="bg-indigo-900/20 dark:bg-indigo-600/30 rounded-xl px-4 py-2 inline-flex items-center">
               <span className="font-medium text-lg md:text-3xl dark:text-neutral-200">
-                Prize pool of 1000
+                Prize pool of 10
               </span>
               <span className="font-semibold text-lg md:text-3xl ml-2 text-amber-500 dark:text-amber-300">
                 SOL
               </span>
             </div>
             <span className="ml-3 text-base md:text-xl text-gray-600 dark:text-gray-400">
-              & other crypto
+              & Airdrops
             </span>
           </div>
           <div className="font-bold text-lg md:text-2xl text-amber-500 dark:text-amber-400 mt-1">
